b6034ab3c1630fe6bc8e6c308767e2168f4db85e
amirrhossein.ir <amirrhossein.ir@gmail.com>
2024-01-26 18:34:14 +0330

    get data as api and show in app

--- src/container/Shop/Shop.jsx
+++ src/container/Shop/Shop.jsx
@@ -1,9 +1,26 @@
 import ProductForm from "../../components/ProductForm/ProductForm.jsx";
 import ProductSearch from "../../components/ProductSearch/ProductSearch.jsx";
 import ProductList from "../../components/ProductList/ProductList.jsx";
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 const Shop=()=>{
     const [products, setProducts]=useState([])
+    useEffect(()=>{
+        fetch("https://practice-react-d0abc-default-rtdb.firebaseio.com/newProducts.json")
+            .then((response)=>{
+                return response.json()
+            })
+            .then((responseDate)=>{
+                const loadedData=[]
+                for (const item in responseDate){
+                    loadedData.push({
+                        id:item,
+                        name:responseDate[item].name,
+                        number:responseDate[item].number
+                    })
+                }
+                setProducts(loadedData)
+            })
+    },[])
     const addProduct=(item)=>{
         fetch("https://practice-react-d0abc-default-rtdb.firebaseio.com/newProducts.json",{
             method:'POST',
