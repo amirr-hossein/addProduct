afcbf64c55ad7403745471d9632da969fc4d5fd7
amirrhossein.ir <amirrhossein.ir@gmail.com>
2024-02-07 11:52:22 +0330

    edit=>add authFirebase fot auth and install firebase

--- src/container/Shop/Shop.jsx
+++ src/container/Shop/Shop.jsx
@@ -1,40 +1,36 @@
-  import ProductForm from "../../components/ProductForm/ProductForm.jsx";
-  import ProductSearch from "../../components/ProductSearch/ProductSearch.jsx";
-  import React, { useCallback } from "react";
-  const Shop = (props) => {
-    const { dispath, product } = props;
-    const addProduct = (item) => {
-      fetch(
-        "https://practice-react-d0abc-default-rtdb.firebaseio.com/newProducts.json",
-        {
-          method: "POST",
-          body: JSON.stringify(item),
-          headers: { "Content-Type": "application/json" },
-        }
-      ).then((response) => {
-        response.json().then((data) => {
-      // data.name در Firebase آیدی است که از سمت سرور تولید شده است
-      const firebaseId = data.name;
-      
-      // اضافه کردن آیدی از Firebase به ایتم
-      dispath({
-        type: "ADD",
-        product: { ...item, id: firebaseId },
-      });
-
+import ProductForm from "../../components/ProductForm/ProductForm.jsx";
+import ProductSearch from "../../components/ProductSearch/ProductSearch.jsx";
+import React, { useCallback } from "react";
+const Shop = (props) => {
+  const { dispath, product } = props;
+  const addProduct = (item) => {
+    fetch(
+      "https://practice-react-d0abc-default-rtdb.firebaseio.com/newProducts.json",
+      {
+        method: "POST",
+        body: JSON.stringify(item),
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then((response) => {
+      response.json().then((data) => {
+        const firebaseId = data.name;
+        dispath({
+          type: "ADD",
+          product: { id: firebaseId,...item },
         });
       });
-    };
-
-    console.log(product)
-    const searchProductHandler = useCallback((items) => {
-      dispath({ type: "SET", products: items });
-    }, []);
-    return (
-      <>
-        <ProductForm add={addProduct} themeBtn={props.themeForm} />
-        <ProductSearch onLoadProducts={searchProductHandler} />
-      </>
-    );
+    });
   };
-  export default Shop;
+
+  console.log(product);
+  const searchProductHandler = useCallback((items) => {
+    dispath({ type: "SET", products: items });
+  }, []);
+  return (
+    <>
+      <ProductForm add={addProduct} themeBtn={props.themeForm} />
+      <ProductSearch onLoadProducts={searchProductHandler} />
+    </>
+  );
+};
+export default Shop;
---
+++ src/firebase/firebase.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAdGlMpvcdl3Pmfq-qhQER1JOqvol2UK4",
+  authDomain: "practice-react-d0abc.firebaseapp.com",
+  databaseURL: "https://practice-react-d0abc-default-rtdb.firebaseio.com",
+  projectId: "practice-react-d0abc",
+  storageBucket: "practice-react-d0abc.appspot.com",
+  messagingSenderId: "19737889458",
+  appId: "1:19737889458:web:70827fc18b06def90557ab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+export {auth}
